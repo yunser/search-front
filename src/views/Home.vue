fb31ca1dc6a016bcbf51da08be5688dea7503534
cjh <1418503647@qq.com>
2018-02-28 01:02:46 +0800

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+    <my-page title="搜索">
+        <ui-text-field v-model="keyword" />
+        <ui-raised-button label="搜索" @click="search" />
+    </my-page>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                keyword: ''
+            }
+        },
+        methods: {
+            search() {
+            }
+        }
+    }
+</script>
+
+<style scoped>
+</style>
